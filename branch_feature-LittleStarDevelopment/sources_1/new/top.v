78941b41dd393e481c4138addd92f2317b2d1e66
WangTaojie <12210519@mail.sustech.edu.cn>
2023-12-18 11:20:43 +0800

    ROM priliminary implemented.

--- branch_feature-LittleStarDevelopment/sources_1/new/top.v
+++ branch_feature-LittleStarDevelopment/sources_1/new/top.v
@@ -1,22 +1,17 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Engineer: Wang Taojie
 // 
 // Create Date: 2023/12/13 23:07:45
 // Design Name: 
 // Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Using Vivado IP core to implement storage.
+// Achieved: Store a piece of notes of Little Star in rom. Functinality verified.
+// Todo next:
+// 1. Complete the whole pieces for Little Star. 
+// 2. Implement recording - write functionality. 
+// 3.* Store the duration for each playing note, instaed of all lasting ```duration_single_note```. 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 `include "para.v"
@@ -27,28 +22,35 @@ module top(
     output speaker,
     output sel
 );
-    assign sel = 1; // TEST ARE CONDUCTED ON EARPHONE, NOT BUZZER.
+    assign sel = 1;
+    parameter duration_single_note = 50000000, pause_duration = 10000000;
 
-    reg [7:0]index; // noteBook[index] sould be the note to play.
-    wire [9:0]bus; // note + pitch. The output from Memory module.
+    reg [6:0]index; // noteBook[index] sould be the note to play.
+    wire [9:0]bus; // note + pitch.
     reg [31:0]counter; // counter for 1 second, each note play 1 sec.
-    reg [31:0] delay_counter; // counter for the spcace between two  consecutive notes.
+    reg [31:0] delay_counter;
     reg stop; // indicating the space between two notes, act as an enable in Buzzer.
+    wire[9:0] length; // The number of notes in the song, stored in rom[0].
 
-    initial begin
-        index = 0;
+    initial begin // initialization
+        index = 1;
         counter = 0;
         delay_counter = 0;
         stop = 0;
     end
 
-     Memory mem(index, bus); // Get the note and pitch from the memory module.
+    // Testing: output the note of index 1
+     LittleStar_Notes nt(.addra(index), .clka(clk), .douta(bus)); 
+     LittleStar_Notes getLength(.addra(0), .clka(clk), .douta(length)); // rom[0] stores the number of notes.
+     // Remark: if you write IP core, note that declare the ports.
+     // LittleStar_Notes nt(index, clk, .bus);  - > this will not work. 
 
+    // Space are added between two notes.
     always @(posedge clk) begin
-        if (counter < 50000000) begin
+        if (counter < duration_single_note) begin
             counter <= counter + 1; 
         end else begin
-            if (delay_counter < 10000000) begin // space between notes.
+            if (delay_counter < pause_duration) begin // space between notes.
                 delay_counter <= delay_counter + 1;
                 stop <= 1;
             end else begin
@@ -56,15 +58,14 @@ module top(
                 stop <= 0;
                 counter <= 0;
                 index <= index + 1; // change the note.
-                if (index > 30) begin
-                    index <= 0; // loop play little stars.
+                if (index > length) begin
+                    index <= 1; // loop play little stars.
                 end
             end
-            
         end
     end
 
-     wire sel_uselss, markLED_useless; // We don't need LED this time.
+     wire sel_uselss, markLED_useless; // LED not needed.
      wire [6:0]note;
      wire [2:0]pitch;
      assign note = bus[9:3]; // seperate the note and pitch.
